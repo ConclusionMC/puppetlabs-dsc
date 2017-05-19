bab69dff5dfbbce5fbfb9ab1c958df4c54bc0a63
Bob Smienk <bsmienk@live.nl>
2017-05-19 14:27:16 +0200

    really added cBAM resource this time

---
+++ lib/puppet_x/dsc_resources/cBAM/DSCResources/cBAM_BatchFile/cBAM_BatchFile.psm1
@@ -0,0 +1,219 @@
+﻿Function Get-TargetResource {
+
+    Param(
+        
+        [Parameter(Mandatory=$True)]
+        [string]$BatchFile,
+
+        [Parameter(Mandatory=$True)]
+        [string[]]$Template,
+
+        [Parameter(Mandatory=$False)]
+        [string]$Auth_Server,
+
+        [Parameter(Mandatory=$False)]
+        [string]$Defect_Server,
+
+        [Parameter(Mandatory=$False)]
+        [string]$Conflict_Server,
+
+        [Parameter(Mandatory=$False)]
+        [string]$Plan_Server,
+
+        [Parameter(Mandatory=$False)]
+        [string]$Main_Server,
+
+        [Parameter(Mandatory=$False)]
+        [string]$JMS_Address,
+
+        [Parameter(Mandatory=$False)]
+        [string]$JRE_Home,
+
+        [Parameter(Mandatory=$False)]
+        [bool]$DesiredState
+
+    )
+    
+    $DesiredState = $True
+
+    $BamDir = "$env:ProgramFiles\BAMClient"
+
+    If (-not (Test-Path "$BamDir\$BatchFile")){ $DesiredState = $False }
+    Else { $Content = Get-Content -Path "$BamDir\$BatchFile" }
+
+    $Configuration = $PSBoundParameters.GetEnumerator() | ? { $_.Key -notmatch 'BatchFile' -and $_.Key -notmatch 'Template' }
+
+    $Commands = @{
+        Auth_Server = 'SET NS_AUTHENTICATION_PROVIDER_ADRES='
+        Defect_Server = 'SET NS_DEFECTENOVERZICHT_ADRES='
+        Conflict_Server = 'SET NS_CONFLICTSIGNALERING_ADRES='
+        Plan_Server = 'SET NS_PLANSERVER_ADRES='
+        Main_Server = 'SET NS_MAINSERVER_ADRES='
+        JMS_Address = 'SET NS_JMS_ADRES='
+        JRE_Home = 'SET JRE_HOME='
+    }
+
+    Foreach ($Config in $Configuration) {
+
+        $Command = $Commands[$Config.Key]
+        $Matches = $Content | Select-String -Pattern $Command
+
+        Foreach ($Match in $Matches) {
+            $CurrentConfig = ($Match.Line -Split '=')[1]
+            If ($CurrentConfig -ne $Config.Value) { $DesiredState = $False }
+        }        
+    }
+
+    Return @{  
+        BatchFile = $BatchFile
+        Template = $Template
+        Auth_Server = $Auth_Server
+        Defect_Server = $Defect_Server
+        Conflict_Server = $Conflict_Server
+        Plan_Server = $Plan_Server
+        Main_Server = $Main_Server
+        JMS_Address = $JMS_Address
+        JRE_Home = $JRE_Home
+        DesiredState    = $DesiredState
+    }
+}
+
+Function Set-TargetResource {
+    
+    Param(
+        
+        [Parameter(Mandatory=$True)]
+        [string]$BatchFile,
+
+        [Parameter(Mandatory=$True)]
+        [string[]]$Template,
+
+        [Parameter(Mandatory=$False)]
+        [string]$Auth_Server,
+
+        [Parameter(Mandatory=$False)]
+        [string]$Defect_Server,
+
+        [Parameter(Mandatory=$False)]
+        [string]$Conflict_Server,
+
+        [Parameter(Mandatory=$False)]
+        [string]$Plan_Server,
+
+        [Parameter(Mandatory=$False)]
+        [string]$Main_Server,
+
+        [Parameter(Mandatory=$False)]
+        [string]$JMS_Address,
+
+        [Parameter(Mandatory=$False)]
+        [string]$JRE_Home,
+
+        [Parameter(Mandatory=$False)]
+        [bool]$DesiredState
+
+    )
+
+    $BamDir = "$env:ProgramFiles\BAMClient"
+
+    If (-not (Test-Path "$BamDir\$BatchFile")){ New-Item -Path "$BamDir\$BatchFile" -ItemType File ; $Content = $Template }
+    Else { $Content = Get-Content -Path "$BamDir\$BatchFile" }
+
+    $Update = $False
+    $Configuration = $PSBoundParameters.GetEnumerator() | ? { $_.Key -notmatch 'BatchFile' -and $_.Key -notmatch 'Template' }
+
+    $Commands = @{
+        Auth_Server = 'SET NS_AUTHENTICATION_PROVIDER_ADRES='
+        Defect_Server = 'SET NS_DEFECTENOVERZICHT_ADRES='
+        Conflict_Server = 'SET NS_CONFLICTSIGNALERING_ADRES='
+        Plan_Server = 'SET NS_PLANSERVER_ADRES='
+        Main_Server = 'SET NS_MAINSERVER_ADRES='
+        JMS_Address = 'SET NS_JMS_ADRES='
+        JRE_Home = 'SET JRE_HOME='
+    }
+
+    Foreach ($Config in $Configuration) {
+
+        $Command = $Commands[$Config.Key]
+        $Matches = $Content | Select-String -Pattern $Command
+        If ($Matches -eq $Null) { Write-Error "No entry for '$($Config.Key)' could be found." ; Continue }
+        Else {
+
+            Foreach ($Match in $Matches) {
+                $CurrentConfig = ($Match.Line -Split '=')[1]
+                If ($CurrentConfig -ne $Config.Value) {
+                    $Content[($Match.LineNumber - 1)] = "$Command$($Config.Value)"
+                    $Update = $True
+                }
+            }
+        }
+    }
+    If ($Update) { Set-Content -Path "$BamDir\$BatchFile" -Value $Content }
+}
+
+Function Test-TargetResource {
+    
+    Param(
+        
+        [Parameter(Mandatory=$True)]
+        [string]$BatchFile,
+
+        [Parameter(Mandatory=$True)]
+        [string[]]$Template,
+
+        [Parameter(Mandatory=$False)]
+        [string]$Auth_Server,
+
+        [Parameter(Mandatory=$False)]
+        [string]$Defect_Server,
+
+        [Parameter(Mandatory=$False)]
+        [string]$Conflict_Server,
+
+        [Parameter(Mandatory=$False)]
+        [string]$Plan_Server,
+
+        [Parameter(Mandatory=$False)]
+        [string]$Main_Server,
+
+        [Parameter(Mandatory=$False)]
+        [string]$JMS_Address,
+
+        [Parameter(Mandatory=$False)]
+        [string]$JRE_Home,
+
+        [Parameter(Mandatory=$False)]
+        [bool]$DesiredState
+
+    )
+
+    $BamDir = "$env:ProgramFiles\BAMClient"
+
+    If (-not (Test-Path "$BamDir\$BatchFile")){ Return $False }
+    Else { $Content = Get-Content -Path "$BamDir\$BatchFile" }
+
+    $Configuration = $PSBoundParameters.GetEnumerator() | ? { $_.Key -notmatch 'BatchFile' -and $_.Key -notmatch 'Template' }
+
+    $Commands = @{
+        Auth_Server = 'SET NS_AUTHENTICATION_PROVIDER_ADRES='
+        Defect_Server = 'SET NS_DEFECTENOVERZICHT_ADRES='
+        Conflict_Server = 'SET NS_CONFLICTSIGNALERING_ADRES='
+        Plan_Server = 'SET NS_PLANSERVER_ADRES='
+        Main_Server = 'SET NS_MAINSERVER_ADRES='
+        JMS_Address = 'SET NS_JMS_ADRES='
+        JRE_Home = 'SET JRE_HOME='
+    }
+
+    Foreach ($Config in $Configuration) {
+
+        $Command = $Commands[$Config.Key]
+        $Matches = $Content | Select-String -Pattern $Command
+
+        Foreach ($Match in $Matches) {
+            $CurrentConfig = ($Match.Line -Split '=')[1]
+            If ($CurrentConfig -ne $Config.Value) { Return $False }
+        }        
+    }
+
+    Return $True
+}
---
+++ lib/puppet_x/dsc_resources/cBAM/cBAM.psd1
@@ -0,0 +1,13 @@
+﻿
+@{
+	Copyright         = "2017 CMC. All rights reserved."; 
+	Description       = "Module containing DSC resources for B@M."; 
+	CompanyName       = "Conclusion Mission Critical"; 
+	GUID              = "a38df075-b0c1-4486-aa94-2f28694e6451"; 
+	PowerShellVersion = "4.0"; 
+	FunctionsToExport = "*"; 
+	CLRVersion        = "4.0"; 
+	CmdletsToExport   = "*"; 
+	Author            = "Bob Smienk"; 
+	ModuleVersion     = "1.0"
+}
