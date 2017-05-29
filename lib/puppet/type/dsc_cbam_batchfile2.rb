043a23813863d263319e12f02ef6f5a844db4bba
Bob Smienk <bsmienk@live.nl>
2017-05-29 09:07:59 +0200

    cBAM update

---
+++ lib/puppet/type/dsc_cbam_batchfile2.rb
@@ -0,0 +1,239 @@
+require 'pathname'
+
+Puppet::Type.newtype(:dsc_cbam_batchfile2) do
+  require Pathname.new(__FILE__).dirname + '../../' + 'puppet/type/base_dsc'
+  require Pathname.new(__FILE__).dirname + '../../puppet_x/puppetlabs/dsc_type_helpers'
+
+
+  @doc = %q{
+    The DSC cBAM_BatchFile2 resource type.
+    Automatically generated from
+    'cBAM/DSCResources/cBAM_BatchFile2/cBAM_BatchFile2.schema.mof'
+
+    To learn more about PowerShell Desired State Configuration, please
+    visit https://technet.microsoft.com/en-us/library/dn249912.aspx.
+
+    For more information about built-in DSC Resources, please visit
+    https://technet.microsoft.com/en-us/library/dn249921.aspx.
+
+    For more information about xDsc Resources, please visit
+    https://github.com/PowerShell/DscResources.
+  }
+
+  validate do
+      fail('dsc_batchfile is a required attribute') if self[:dsc_batchfile].nil?
+    end
+
+  def dscmeta_resource_friendly_name; 'cBAM_BatchFile2' end
+  def dscmeta_resource_name; 'cBAM_BatchFile2' end
+  def dscmeta_module_name; 'cBAM' end
+  def dscmeta_module_version; '1.0' end
+
+  newparam(:name, :namevar => true ) do
+  end
+
+  ensurable do
+    newvalue(:exists?) { provider.exists? }
+    newvalue(:present) { provider.create }
+    defaultto { :present }
+  end
+
+  # Name:         PsDscRunAsCredential
+  # Type:         MSFT_Credential
+  # IsMandatory:  False
+  # Values:       None
+  newparam(:dsc_psdscrunascredential) do
+    def mof_type; 'MSFT_Credential' end
+    def mof_is_embedded?; true end
+    desc "PsDscRunAsCredential"
+    validate do |value|
+      unless value.kind_of?(Hash)
+        fail("Invalid value '#{value}'. Should be a hash")
+      end
+      PuppetX::Dsc::TypeHelpers.validate_MSFT_Credential("Credential", value)
+    end
+  end
+
+  # Name:         BatchFile
+  # Type:         string
+  # IsMandatory:  True
+  # Values:       None
+  newparam(:dsc_batchfile) do
+    def mof_type; 'string' end
+    def mof_is_embedded?; false end
+    desc "BatchFile"
+    isrequired
+    validate do |value|
+      unless value.kind_of?(String)
+        fail("Invalid value '#{value}'. Should be a string")
+      end
+    end
+  end
+
+  # Name:         Template
+  # Type:         string[]
+  # IsMandatory:  False
+  # Values:       None
+  newparam(:dsc_template, :array_matching => :all) do
+    def mof_type; 'string[]' end
+    def mof_is_embedded?; false end
+    desc "Template"
+    validate do |value|
+      unless value.kind_of?(Array) || value.kind_of?(String)
+        fail("Invalid value '#{value}'. Should be a string or an array of strings")
+      end
+    end
+    munge do |value|
+      Array(value)
+    end
+  end
+
+  # Name:         StartCommand
+  # Type:         string
+  # IsMandatory:  False
+  # Values:       None
+  newparam(:dsc_startcommand) do
+    def mof_type; 'string' end
+    def mof_is_embedded?; false end
+    desc "StartCommand"
+    validate do |value|
+      unless value.kind_of?(String)
+        fail("Invalid value '#{value}'. Should be a string")
+      end
+    end
+  end
+
+  # Name:         Auth_Server
+  # Type:         string
+  # IsMandatory:  False
+  # Values:       None
+  newparam(:dsc_auth_server) do
+    def mof_type; 'string' end
+    def mof_is_embedded?; false end
+    desc "Auth_Server"
+    validate do |value|
+      unless value.kind_of?(String)
+        fail("Invalid value '#{value}'. Should be a string")
+      end
+    end
+  end
+
+  # Name:         Defect_Server
+  # Type:         string
+  # IsMandatory:  False
+  # Values:       None
+  newparam(:dsc_defect_server) do
+    def mof_type; 'string' end
+    def mof_is_embedded?; false end
+    desc "Defect_Server"
+    validate do |value|
+      unless value.kind_of?(String)
+        fail("Invalid value '#{value}'. Should be a string")
+      end
+    end
+  end
+
+  # Name:         Conflict_Server
+  # Type:         string
+  # IsMandatory:  False
+  # Values:       None
+  newparam(:dsc_conflict_server) do
+    def mof_type; 'string' end
+    def mof_is_embedded?; false end
+    desc "Conflict_Server"
+    validate do |value|
+      unless value.kind_of?(String)
+        fail("Invalid value '#{value}'. Should be a string")
+      end
+    end
+  end
+
+  # Name:         Plan_Server
+  # Type:         string
+  # IsMandatory:  False
+  # Values:       None
+  newparam(:dsc_plan_server) do
+    def mof_type; 'string' end
+    def mof_is_embedded?; false end
+    desc "Plan_Server"
+    validate do |value|
+      unless value.kind_of?(String)
+        fail("Invalid value '#{value}'. Should be a string")
+      end
+    end
+  end
+
+  # Name:         Main_Server
+  # Type:         string
+  # IsMandatory:  False
+  # Values:       None
+  newparam(:dsc_main_server) do
+    def mof_type; 'string' end
+    def mof_is_embedded?; false end
+    desc "Main_Server"
+    validate do |value|
+      unless value.kind_of?(String)
+        fail("Invalid value '#{value}'. Should be a string")
+      end
+    end
+  end
+
+  # Name:         JMS_Address
+  # Type:         string
+  # IsMandatory:  False
+  # Values:       None
+  newparam(:dsc_jms_address) do
+    def mof_type; 'string' end
+    def mof_is_embedded?; false end
+    desc "JMS_Address"
+    validate do |value|
+      unless value.kind_of?(String)
+        fail("Invalid value '#{value}'. Should be a string")
+      end
+    end
+  end
+
+  # Name:         JRE_Home
+  # Type:         string
+  # IsMandatory:  False
+  # Values:       None
+  newparam(:dsc_jre_home) do
+    def mof_type; 'string' end
+    def mof_is_embedded?; false end
+    desc "JRE_Home"
+    validate do |value|
+      unless value.kind_of?(String)
+        fail("Invalid value '#{value}'. Should be a string")
+      end
+    end
+  end
+
+  # Name:         DesiredState
+  # Type:         boolean
+  # IsMandatory:  False
+  # Values:       None
+  newparam(:dsc_desiredstate) do
+    def mof_type; 'boolean' end
+    def mof_is_embedded?; false end
+    desc "DesiredState"
+    validate do |value|
+    end
+    newvalues(true, false)
+    munge do |value|
+      PuppetX::Dsc::TypeHelpers.munge_boolean(value.to_s)
+    end
+  end
+
+
+  def builddepends
+    pending_relations = super()
+    PuppetX::Dsc::TypeHelpers.ensure_reboot_relationship(self, pending_relations)
+  end
+end
+
+Puppet::Type.type(:dsc_cbam_batchfile2).provide :powershell, :parent => Puppet::Type.type(:base_dsc).provider(:powershell) do
+  confine :true => (Gem::Version.new(Facter.value(:powershell_version)) >= Gem::Version.new('5.0.10240.16384'))
+  defaultfor :operatingsystem => :windows
+
+  mk_resource_methods
+end
---
+++ lib/puppet_x/dsc_resources/cBAM/DSCResources/cBAM_BatchFile2/cBAM_BatchFile2.psm1
@@ -0,0 +1,207 @@
+﻿Function Get-TargetResource {
+
+    Param(
+        
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string]$BatchFile,
+
+        [Parameter(Mandatory=$True)]
+        [string[]]$Template,
+
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string]$StartCommand,
+
+        [Parameter(Mandatory=$False)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Auth_Server,
+
+        [Parameter(Mandatory=$False)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Defect_Server,
+
+        [Parameter(Mandatory=$False)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Conflict_Server,
+
+        [Parameter(Mandatory=$False)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Plan_Server,
+
+        [Parameter(Mandatory=$False)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Main_Server,
+
+        [Parameter(Mandatory=$False)]
+        [ValidateNotNullOrEmpty()]
+        [string]$JMS_Address,
+
+        [Parameter(Mandatory=$False)]
+        [ValidateNotNullOrEmpty()]
+        [string]$JRE_Home,
+
+        [Parameter(Mandatory=$False)]
+        [bool]$DesiredState
+
+    )    
+
+    $DesiredState = $True
+
+    $Template = $Template -replace '<Blank>',''
+
+    $StartSettings = $Template | Select-String '<Settings>'
+    If ($StartSettings -eq $Null) { Throw "No settings segment could be found in the template. Pleas include the '<Settings>' and '</Settings>' keywords in your template." }
+    Elseif ($StartSettings.Count -gt 1) { Throw "More than one '<Settings>' keyword was found." }
+    $EndSettings = $Template | Select-String '</Settings>'
+    If ($EndSettings -eq $Null) { Throw "No settings segment could be found in the template. Pleas include the '<Settings>' and '</Settings>' keywords in your template." }
+    Elseif ($EndSettings.Count -gt 1) { Throw "More than one '</Settings>' keyword was found." }
+    If ($StartSettings.LineNumber -ge $EndSettings.LineNumber) { Throw "The '</Settings>' keyword can not be on the same line or before the '<Settings>' keyword." }
+
+    $BamDir = 'C:\Program Files\BAMClient'
+
+    If (-not (Test-Path -Path "$BamDir\$BatchFile")) { $FileExists = $False ; $DesiredState = $False ; $CurrentContent = @() }
+    Else { $FileExists = $True ; $CurrentContent = Get-Content "$BamDir\$BatchFile" }
+    
+    $BeforeSettings = $Template[0..($StartSettings.LineNumber - 1)]
+    $AfterSettings = $Template[($EndSettings.LineNumber - 1)..($Template.Count - 1)]
+    If ($AfterSettings[-1] -eq '<StartCommand>') { $AfterSettings[-1] = $StartCommand }
+    Elseif ($AfterSettings[-1].Contains('start "" eclipse.exe') -and ($AfterSettings[-1] -ne $StartCommand)) { $AfterSettings[-1] = $StartCommand }
+    Elseif ($AfterSettings[-1] -ne $StartCommand) { $AfterSettings += $StartCommand }
+    
+    $Commands = @{
+        Auth_Server = 'SET NS_AUTHENTICATION_PROVIDER_ADRES='
+        Defect_Server = 'SET NS_DEFECTENOVERZICHT_ADRES='
+        Conflict_Server = 'SET NS_CONFLICTSIGNALERING_ADRES='
+        Plan_Server = 'SET NS_PLANSERVER_ADRES='
+        Main_Server = 'SET NS_MAINSERVER_ADRES='
+        JMS_Address = 'SET NS_JMS_ADRES='
+        JRE_Home = 'SET JRE_HOME='
+    }
+
+    $RequiredSettings = @()
+    Foreach ($Command in ($Commands.GetEnumerator() | Sort Key)) { If ($PSBoundParameters.ContainsKey($Command.Key)) { $RequiredSettings += "$($Command.Value)$($PSBoundParameters[$Command.Key])" } }
+
+    $RequiredContent = $BeforeSettings + $RequiredSettings + $AfterSettings
+
+    If (($CurrentContent -ne $Null) -and ($CurrentContent.Count -ne 0)) {
+        $Compare = Compare-Object -ReferenceObject $CurrentContent -DifferenceObject $RequiredContent
+        If ($Compare -ne $Null) { $DesiredState = $False }
+    }
+    Else { $DesiredState = $False }
+
+    Return @{  
+        BamDir = $BamDir
+        FileExists = $FileExists
+        CurrentContent = $CurrentContent
+        RequiredContent = $RequiredContent
+        DesiredState    = $DesiredState
+    }
+}
+
+Function Set-TargetResource {
+    
+    Param(
+        
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string]$BatchFile,
+
+        [Parameter(Mandatory=$True)]
+        [string[]]$Template,
+
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string]$StartCommand,
+
+        [Parameter(Mandatory=$False)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Auth_Server,
+
+        [Parameter(Mandatory=$False)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Defect_Server,
+
+        [Parameter(Mandatory=$False)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Conflict_Server,
+
+        [Parameter(Mandatory=$False)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Plan_Server,
+
+        [Parameter(Mandatory=$False)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Main_Server,
+
+        [Parameter(Mandatory=$False)]
+        [ValidateNotNullOrEmpty()]
+        [string]$JMS_Address,
+
+        [Parameter(Mandatory=$False)]
+        [ValidateNotNullOrEmpty()]
+        [string]$JRE_Home,
+
+        [Parameter(Mandatory=$False)]
+        [bool]$DesiredState
+
+    )    
+
+    $CurrentState = Get-TargetResource @PSBoundParameters
+    If ($CurrentState.FileExists -eq $False) { $File = New-Item -Path "$($CurrentState.BamDir)\$BatchFile" -Value ($CurrentState.RequiredContent | Out-String) }
+    Else { Set-Content -Path "$($CurrentState.BamDir)\$BatchFile" -Value $CurrentState.RequiredContent }
+
+}
+
+Function Test-TargetResource {
+    
+    Param(
+        
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string]$BatchFile,
+
+        [Parameter(Mandatory=$True)]
+        [string[]]$Template,
+
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string]$StartCommand,
+
+        [Parameter(Mandatory=$False)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Auth_Server,
+
+        [Parameter(Mandatory=$False)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Defect_Server,
+
+        [Parameter(Mandatory=$False)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Conflict_Server,
+
+        [Parameter(Mandatory=$False)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Plan_Server,
+
+        [Parameter(Mandatory=$False)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Main_Server,
+
+        [Parameter(Mandatory=$False)]
+        [ValidateNotNullOrEmpty()]
+        [string]$JMS_Address,
+
+        [Parameter(Mandatory=$False)]
+        [ValidateNotNullOrEmpty()]
+        [string]$JRE_Home,
+
+        [Parameter(Mandatory=$False)]
+        [bool]$DesiredState
+
+    )    
+
+    $CurrentState = Get-TargetResource @PSBoundParameters
+    Return $CurrentState.DesiredState
+
+}
