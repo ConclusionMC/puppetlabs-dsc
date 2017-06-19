bde44321b3dbe48303ac9c334cfdf9a0364e929a
Bob Smienk <bsmienk@live.nl>
2017-06-19 10:38:16 +0200

    added resource to rename interface

---
+++ lib/puppet/type/dsc_cmisc_rename_interface.rb
@@ -0,0 +1,118 @@
+require 'pathname'
+
+Puppet::Type.newtype(:dsc_cmisc_rename_interface) do
+  require Pathname.new(__FILE__).dirname + '../../' + 'puppet/type/base_dsc'
+  require Pathname.new(__FILE__).dirname + '../../puppet_x/puppetlabs/dsc_type_helpers'
+
+
+  @doc = %q{
+    The DSC cMisc_Rename_Interface resource type.
+    Automatically generated from
+    'cMisc/DSCResources/cMisc_Rename_Interface/cMisc_Rename_Interface.schema.mof'
+
+    To learn more about PowerShell Desired State Configuration, please
+    visit https://technet.microsoft.com/en-us/library/dn249912.aspx.
+
+    For more information about built-in DSC Resources, please visit
+    https://technet.microsoft.com/en-us/library/dn249921.aspx.
+
+    For more information about xDsc Resources, please visit
+    https://github.com/PowerShell/DscResources.
+  }
+
+  validate do
+      fail('dsc_currentname is a required attribute') if self[:dsc_currentname].nil?
+      fail('dsc_newname is a required attribute') if self[:dsc_newname].nil?
+    end
+
+  def dscmeta_resource_friendly_name; 'cMisc_Rename_Interface' end
+  def dscmeta_resource_name; 'cMisc_Rename_Interface' end
+  def dscmeta_module_name; 'cMisc' end
+  def dscmeta_module_version; '1.0' end
+
+  newparam(:name, :namevar => true ) do
+  end
+
+  ensurable do
+    newvalue(:exists?) { provider.exists? }
+    newvalue(:present) { provider.create }
+    defaultto { :present }
+  end
+
+  # Name:         PsDscRunAsCredential
+  # Type:         MSFT_Credential
+  # IsMandatory:  False
+  # Values:       None
+  newparam(:dsc_psdscrunascredential) do
+    def mof_type; 'MSFT_Credential' end
+    def mof_is_embedded?; true end
+    desc "PsDscRunAsCredential"
+    validate do |value|
+      unless value.kind_of?(Hash)
+        fail("Invalid value '#{value}'. Should be a hash")
+      end
+      PuppetX::Dsc::TypeHelpers.validate_MSFT_Credential("Credential", value)
+    end
+  end
+
+  # Name:         CurrentName
+  # Type:         string
+  # IsMandatory:  True
+  # Values:       None
+  newparam(:dsc_currentname) do
+    def mof_type; 'string' end
+    def mof_is_embedded?; false end
+    desc "CurrentName"
+    isrequired
+    validate do |value|
+      unless value.kind_of?(String)
+        fail("Invalid value '#{value}'. Should be a string")
+      end
+    end
+  end
+
+  # Name:         NewName
+  # Type:         string
+  # IsMandatory:  True
+  # Values:       None
+  newparam(:dsc_newname) do
+    def mof_type; 'string' end
+    def mof_is_embedded?; false end
+    desc "NewName"
+    isrequired
+    validate do |value|
+      unless value.kind_of?(String)
+        fail("Invalid value '#{value}'. Should be a string")
+      end
+    end
+  end
+
+  # Name:         DesiredState
+  # Type:         boolean
+  # IsMandatory:  False
+  # Values:       None
+  newparam(:dsc_desiredstate) do
+    def mof_type; 'boolean' end
+    def mof_is_embedded?; false end
+    desc "DesiredState"
+    validate do |value|
+    end
+    newvalues(true, false)
+    munge do |value|
+      PuppetX::Dsc::TypeHelpers.munge_boolean(value.to_s)
+    end
+  end
+
+
+  def builddepends
+    pending_relations = super()
+    PuppetX::Dsc::TypeHelpers.ensure_reboot_relationship(self, pending_relations)
+  end
+end
+
+Puppet::Type.type(:dsc_cmisc_rename_interface).provide :powershell, :parent => Puppet::Type.type(:base_dsc).provider(:powershell) do
+  confine :true => (Gem::Version.new(Facter.value(:powershell_version)) >= Gem::Version.new('5.0.10240.16384'))
+  defaultfor :operatingsystem => :windows
+
+  mk_resource_methods
+end
---
+++ lib/puppet_x/dsc_resources/cMisc/DSCResources/cMisc_Rename_Interface/cMisc_Rename_Interface.psm1
@@ -0,0 +1,69 @@
+﻿Function Get-TargetResource {
+
+    Param(
+        
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string]$CurrentName,
+
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string]$NewName,
+
+        [Parameter(Mandatory=$False)]
+        [bool]$DesiredState
+
+    )
+
+    Try  { 
+        $Interface = Get-NetIPInterface -InterfaceAlias $CurrentName -ErrorAction Stop
+        If ($Interface -ne $Null) { Return @{ DesiredState = $False } }
+    }
+    Catch { 
+        Try { 
+            $Interface = Get-NetIPInterface -InterfaceAlias $NewName -ErrorAction Stop 
+            If ($Interface -ne $Null) { Return @{ DesiredState = $True } }
+        }
+        Catch { Throw "No interface with alias $CurrentName or $NewName could be found." }
+    }
+}
+
+Function Set-TargetResource {
+    
+    Param(
+        
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string]$CurrentName,
+
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string]$NewName,
+
+        [Parameter(Mandatory=$False)]
+        [bool]$DesiredState
+
+    )
+
+    Rename-NetAdapter -Name $CurrentName -NewName $newName
+}
+
+Function Test-TargetResource {
+    
+    Param(
+        
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string]$CurrentName,
+
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string]$NewName,
+
+        [Parameter(Mandatory=$False)]
+        [bool]$DesiredState
+
+    )
+
+    Return (Get-TargetResource @PSBoundParameters).DesiredState
+}
