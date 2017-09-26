4e2567e073f1d503d7b0fd2dcffbdb2821452470
Bob Smienk <bsmienk@live.nl>
2017-09-26 09:48:08 +0200

    Update cMisc_SetWSUS.psm1

--- lib/puppet_x/dsc_resources/cMisc/DSCResources/cMisc_SetWSUS/cMisc_SetWSUS.psm1
+++ lib/puppet_x/dsc_resources/cMisc/DSCResources/cMisc_SetWSUS/cMisc_SetWSUS.psm1
@@ -457,7 +457,7 @@ Function Set-TargetResource {
     )
 
     $CurrentState = Get-TargetResource @PSBoundParameters
-    Foreach ($Path in $Currenstate.ToCreate) {  
+    Foreach ($Path in $Currentstate.ToCreate) {  
         
         $Split = $Path -split '\\'
         Foreach ($SubPath in $Split) {
