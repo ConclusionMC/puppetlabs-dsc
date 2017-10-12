bb8aa6718b947524627a2dabe914dc111cebfae5
Bob Smienk <bsmienk@live.nl>
2017-10-12 09:44:34 +0200

    added resource to cmisc to activate windows

---
+++ lib/puppet_x/dsc_resources/cMisc/DSCResources/cMisc_ActivateWindows/cMisc_ActivateWindows.psm1
@@ -0,0 +1,37 @@
+﻿Function Get-TargetResource {
+
+    Param (
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [String]$IpkKey
+    )
+
+    $Licensestatus = ((cscript slmgr.vbs /dlv | Select-String 'License Status:') -split ': ')[1]
+
+    Return @{
+        Licensestatus = $Licensestatus
+        DesiredState = $Licensestatus -eq "Licensed"
+    }
+}
+
+Function Set-TargetResource {
+
+    Param (
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [String]$IpkKey
+    )
+
+    cscript slmgr.vbs /ipk $IpkKey
+}
+
+Function Test-TargetResource {
+
+    Param (
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [String]$IpkKey
+    )
+
+    Return (Get-TargetResource @PSBoundParameters).DesiredState
+}
