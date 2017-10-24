55460dccd39b38502770a5df2a8753e181b83d86
Bob Smienk <bsmienk@live.nl>
2017-10-24 10:39:58 +0200

    added resource to set cluster subnet delays

---
+++ lib/puppet/type/dsc_cmisc_clustersubnetdelay.rb
@@ -0,0 +1,108 @@
+require 'pathname'
+
+Puppet::Type.newtype(:dsc_cmisc_clustersubnetdelay) do
+  require Pathname.new(__FILE__).dirname + '../../' + 'puppet/type/base_dsc'
+  require Pathname.new(__FILE__).dirname + '../../puppet_x/puppetlabs/dsc_type_helpers'
+
+
+  @doc = %q{
+    The DSC cMisc_ClusterSubnetDelay resource type.
+    Automatically generated from
+    'cMisc/DSCResources/cMisc_ClusterSubnetDelay/cMisc_ClusterSubnetDelay.schema.mof'
+
+    To learn more about PowerShell Desired State Configuration, please
+    visit https://technet.microsoft.com/en-us/library/dn249912.aspx.
+
+    For more information about built-in DSC Resources, please visit
+    https://technet.microsoft.com/en-us/library/dn249921.aspx.
+
+    For more information about xDsc Resources, please visit
+    https://github.com/PowerShell/DscResources.
+  }
+
+  validate do
+      fail('dsc_samesubnet is a required attribute') if self[:dsc_samesubnet].nil?
+      fail('dsc_crosssubnet is a required attribute') if self[:dsc_crosssubnet].nil?
+    end
+
+  def dscmeta_resource_friendly_name; 'cMisc_ClusterSubnetDelay' end
+  def dscmeta_resource_name; 'cMisc_ClusterSubnetDelay' end
+  def dscmeta_module_name; 'cMisc' end
+  def dscmeta_module_version; '1.0' end
+
+  newparam(:name, :namevar => true ) do
+  end
+
+  ensurable do
+    newvalue(:exists?) { provider.exists? }
+    newvalue(:present) { provider.create }
+    defaultto { :present }
+  end
+
+  # Name:         PsDscRunAsCredential
+  # Type:         MSFT_Credential
+  # IsMandatory:  False
+  # Values:       None
+  newparam(:dsc_psdscrunascredential) do
+    def mof_type; 'MSFT_Credential' end
+    def mof_is_embedded?; true end
+    desc "PsDscRunAsCredential"
+    validate do |value|
+      unless value.kind_of?(Hash)
+        fail("Invalid value '#{value}'. Should be a hash")
+      end
+      PuppetX::Dsc::TypeHelpers.validate_MSFT_Credential("Credential", value)
+    end
+  end
+
+  # Name:         SameSubnet
+  # Type:         uint32
+  # IsMandatory:  True
+  # Values:       None
+  newparam(:dsc_samesubnet) do
+    def mof_type; 'uint32' end
+    def mof_is_embedded?; false end
+    desc "SameSubnet"
+    isrequired
+    validate do |value|
+      unless (value.kind_of?(Numeric) && value >= 0) || (value.to_i.to_s == value && value.to_i >= 0)
+          fail("Invalid value #{value}. Should be a unsigned Integer")
+      end
+    end
+    munge do |value|
+      PuppetX::Dsc::TypeHelpers.munge_integer(value)
+    end
+  end
+
+  # Name:         CrossSubnet
+  # Type:         uint32
+  # IsMandatory:  True
+  # Values:       None
+  newparam(:dsc_crosssubnet) do
+    def mof_type; 'uint32' end
+    def mof_is_embedded?; false end
+    desc "CrossSubnet"
+    isrequired
+    validate do |value|
+      unless (value.kind_of?(Numeric) && value >= 0) || (value.to_i.to_s == value && value.to_i >= 0)
+          fail("Invalid value #{value}. Should be a unsigned Integer")
+      end
+    end
+    munge do |value|
+      PuppetX::Dsc::TypeHelpers.munge_integer(value)
+    end
+  end
+
+
+  def builddepends
+    pending_relations = super()
+    PuppetX::Dsc::TypeHelpers.ensure_reboot_relationship(self, pending_relations)
+  end
+end
+
+Puppet::Type.type(:dsc_cmisc_clustersubnetdelay).provide :powershell, :parent => Puppet::Type.type(:base_dsc).provider(:powershell) do
+  confine :true => (Gem::Version.new(Facter.value(:powershell_version)) >= Gem::Version.new('5.0.10240.16384'))
+  defaultfor :operatingsystem => :windows
+
+  mk_resource_methods
+end
---
+++ lib/puppet_x/dsc_resources/cMisc/DSCResources/cMisc_ClusterSubnetDelay/cMisc_ClusterSubnetDelay.psm1
@@ -0,0 +1,68 @@
+﻿Function Get-TargetResource {
+
+    Param(
+
+        [Parameter(Mandatory=$True)]
+        [ValidateRange(1000,2000)]
+        [Uint32]$SameSubnet,
+
+        [Parameter(Mandatory=$True)]
+        [ValidateRange(1000,4000)]
+        [Uint32]$CrossSubnet
+
+    )
+
+    $Cluster = Get-Cluster -Verbose:$False
+
+
+    If (($Cluster.CrossSubnetDelay -ne $CrossSubnet) -or ($Cluster.SameSubnetDelay -ne $SameSubnet)) { $DesiredState = $False }
+    Else { $DesiredState = $True }
+
+    Return @{
+        DesiredState = $DesiredState
+        Cluster = $Cluster
+    }
+}
+
+Function Set-TargetResource {
+    
+    Param(
+
+        [Parameter(Mandatory=$True)]
+        [ValidateRange(1000,2000)]
+        [Uint32]$SameSubnet,
+
+        [Parameter(Mandatory=$True)]
+        [ValidateRange(1000,4000)]
+        [Uint32]$CrossSubnet
+
+    )
+
+    $CurrentState = Get-TargetResource @PSBoundParameters
+    If ($CurrentState.Cluster.CurrentSameSubnet -ne $SameSubnet) { 
+        Write-Verbose "Setting SameSubnetDelay to $SameSubnet"
+        $CurrentState.Cluster.SameSubnetDelay = $SameSubnet
+    }
+    
+    If ($CurrentState.Cluster.CurrentCrossSubnet -ne $CrossSubnet) { 
+        Write-Verbose "Setting CrossSubnetDelay to $CrossSubnet"
+        $CurrentState.Cluster.CrossSubnetDelay = $CrossSubnet 
+    }
+}
+
+Function Test-TargetResource {
+    
+    Param(
+
+        [Parameter(Mandatory=$True)]
+        [ValidateRange(1000,2000)]
+        [Uint32]$SameSubnet,
+
+        [Parameter(Mandatory=$True)]
+        [ValidateRange(1000,4000)]
+        [Uint32]$CrossSubnet
+
+    )
+
+    Return (Get-TargetResource @PSBoundParameters).DesiredState
+}
