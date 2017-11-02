adfb32f5c4af8eb9ab619130cfc67e60d7709ba7
Bob Smienk <bsmienk@live.nl>
2017-11-02 15:52:00 +0100

    bugfix for 8dot3name resource

--- lib/puppet_x/dsc_resources/cMisc/DSCResources/cMisc_Disk_Set8dot3/cMisc_Disk_Set8dot3.psm1
+++ lib/puppet_x/dsc_resources/cMisc/DSCResources/cMisc_Disk_Set8dot3/cMisc_Disk_Set8dot3.psm1
@@ -14,6 +14,7 @@
 
     If ($Disk.EndsWith(':')) { $Disk = $Disk.Replace(':','') }
     $Info = fsutil.exe 8dot3name query ${Disk}:
+    If ($Info[0] -match "cannot find the file specified") { Return @{ DesiredState = $True } }
     $CurrentRegistryState = ((($Info | Select-String "registry state") -split ": ")[1] -split " ")[0]
     $CurrentVolumeState = ((($Info | Select-String "volume state") -split ": ")[1] -split " ")[0]
 
@@ -46,9 +47,9 @@ Function Set-TargetResource {
     )
 
     $CurrentState = Get-TargetResource @PSBoundParameters
-    $Disk = $CurrentState.Disk
     If ($CurrentState.CurrentRegistryState -ne "2") { fsutil.exe 8dot3name set 2 }
-    If ($CurrentState.CurrentVolumeState -ne $VolumeState) { fsutil.exe 8dot3name set ${Disk}: $VolumeState }
+    If ($CurrentState.CurrentVolumeState -ne $VolumeState) { fsutil.exe 8dot3name set $($CurrentState.Disk): $VolumeState }
+
 }
 
 Function Test-TargetResource {
