824ee7e40845af6044189636d0856ab3e3fa7e7b
Bob Smienk <bsmienk@live.nl>
2017-11-28 10:07:55 +0100

    improved cmisc_failoverfileserver

--- lib/puppet/type/dsc_cmisc_failoverfileserver2.rb
+++ lib/puppet/type/dsc_cmisc_failoverfileserver2.rb
@@ -1,14 +1,14 @@
 require 'pathname'
 
-Puppet::Type.newtype(:dsc_cmisc_failoverfileserver) do
+Puppet::Type.newtype(:dsc_cmisc_failoverfileserver2) do
   require Pathname.new(__FILE__).dirname + '../../' + 'puppet/type/base_dsc'
   require Pathname.new(__FILE__).dirname + '../../puppet_x/puppetlabs/dsc_type_helpers'
 
 
   @doc = %q{
-    The DSC cMisc_FailoverFileServer resource type.
+    The DSC cMisc_FailoverFileServer2 resource type.
     Automatically generated from
-    'cMisc/DSCResources/cMisc_FailoverFileServer/cMisc_FailoverFileServer.schema.mof'
+    'cMisc/DSCResources/cMisc_FailoverFileServer2/cMisc_FailoverFileServer2.schema.mof'
 
     To learn more about PowerShell Desired State Configuration, please
     visit https://technet.microsoft.com/en-us/library/dn249912.aspx.
@@ -24,8 +24,8 @@ Puppet::Type.newtype(:dsc_cmisc_failoverfileserver) do
       fail('dsc_sharename is a required attribute') if self[:dsc_sharename].nil?
     end
 
-  def dscmeta_resource_friendly_name; 'cMisc_FailoverFileServer' end
-  def dscmeta_resource_name; 'cMisc_FailoverFileServer' end
+  def dscmeta_resource_friendly_name; 'cMisc_FailoverFileServer2' end
+  def dscmeta_resource_name; 'cMisc_FailoverFileServer2' end
   def dscmeta_module_name; 'cMisc' end
   def dscmeta_module_version; '1.0' end
 
@@ -122,7 +122,7 @@ Puppet::Type.newtype(:dsc_cmisc_failoverfileserver) do
   end
 end
 
-Puppet::Type.type(:dsc_cmisc_failoverfileserver).provide :powershell, :parent => Puppet::Type.type(:base_dsc).provider(:powershell) do
+Puppet::Type.type(:dsc_cmisc_failoverfileserver2).provide :powershell, :parent => Puppet::Type.type(:base_dsc).provider(:powershell) do
   confine :true => (Gem::Version.new(Facter.value(:powershell_version)) >= Gem::Version.new('5.0.10240.16384'))
   defaultfor :operatingsystem => :windows
 
