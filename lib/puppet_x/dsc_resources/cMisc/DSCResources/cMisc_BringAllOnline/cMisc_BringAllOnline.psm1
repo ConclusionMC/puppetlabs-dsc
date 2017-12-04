00ae4c956e8e8f50df5cd0cbd7f953e86ba44e5b
Bob Smienk <bsmienk@live.nl>
2017-12-04 14:49:48 +0100

    added bringallonline resource

---
+++ lib/puppet_x/dsc_resources/cMisc/DSCResources/cMisc_BringAllOnline/cMisc_BringAllOnline.psm1
@@ -0,0 +1,52 @@
+﻿Function Get-TargetResource {
+
+    Param(
+        
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string]$GroupName
+
+    )
+
+    $ClusterGroup = Get-ClusterGroup -Name $GroupName -ErrorAction SilentlyContinue
+    If ($ClusterGroup -eq $Null) { Throw "$GroupName could not be contacted" }
+    $CurrentOwner = $ClusterGroup.OwnerNode.NodeName -eq $env:COMPUTERNAME
+    
+    If ($CurrentOwner) {
+        $Resources = $ClusterGroup | Get-ClusterResource
+        If ($Resources.State -contains "Failed" -or $Resources.State -contains "Offline") { Return $False }
+    }
+
+    Return $True
+
+} 
+
+
+Function Set-TargetResource {
+    
+    Param(
+        
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string]$GroupName
+
+    )
+
+    $ClusterGroup = Get-ClusterGroup -Name $GroupName
+    $ClusterGroup | Get-ClusterResource | % { $_ | Start-ClusterResource }
+
+    
+}
+
+Function Test-TargetResource {
+    
+    Param(
+        
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string]$GroupName
+
+    )
+
+    Return Get-TargetResource @PSBoundParameters
+}
