02d06272e36965f25bb7ee99fc6bc597847b5be9
Bob Smienk <bsmienk@live.nl>
2018-02-13 18:54:05 +0100

    added localgroup2

---
+++ lib/puppet/type/dsc_cmisc_localgroup2.rb
@@ -0,0 +1,171 @@
+require 'pathname'
+
+Puppet::Type.newtype(:dsc_cmisc_localgroup2) do
+  require Pathname.new(__FILE__).dirname + '../../' + 'puppet/type/base_dsc'
+  require Pathname.new(__FILE__).dirname + '../../puppet_x/puppetlabs/dsc_type_helpers'
+
+
+  @doc = %q{
+    The DSC cMisc_LocalGroup2 resource type.
+    Automatically generated from
+    'cMisc/DSCResources/cMisc_LocalGroup2/cMisc_LocalGroup2.schema.mof'
+
+    To learn more about PowerShell Desired State Configuration, please
+    visit https://technet.microsoft.com/en-us/library/dn249912.aspx.
+
+    For more information about built-in DSC Resources, please visit
+    https://technet.microsoft.com/en-us/library/dn249921.aspx.
+
+    For more information about xDsc Resources, please visit
+    https://github.com/PowerShell/DscResources.
+  }
+
+  validate do
+      fail('dsc_group is a required attribute') if self[:dsc_group].nil?
+    end
+
+  def dscmeta_resource_friendly_name; 'cMisc_LocalGroup2' end
+  def dscmeta_resource_name; 'cMisc_LocalGroup2' end
+  def dscmeta_module_name; 'cMisc' end
+  def dscmeta_module_version; '1.0' end
+
+  newparam(:name, :namevar => true ) do
+  end
+
+  ensurable do
+    newvalue(:exists?) { provider.exists? }
+    newvalue(:present) { provider.create }
+    newvalue(:absent)  { provider.destroy }
+    defaultto { :present }
+  end
+
+  # Name:         PsDscRunAsCredential
+  # Type:         MSFT_Credential
+  # IsMandatory:  False
+  # Values:       None
+  newparam(:dsc_psdscrunascredential) do
+    def mof_type; 'MSFT_Credential' end
+    def mof_is_embedded?; true end
+    desc "PsDscRunAsCredential"
+    validate do |value|
+      unless value.kind_of?(Hash)
+        fail("Invalid value '#{value}'. Should be a hash")
+      end
+      PuppetX::Dsc::TypeHelpers.validate_MSFT_Credential("Credential", value)
+    end
+  end
+
+  # Name:         Group
+  # Type:         string
+  # IsMandatory:  True
+  # Values:       None
+  newparam(:dsc_group) do
+    def mof_type; 'string' end
+    def mof_is_embedded?; false end
+    desc "Group"
+    isrequired
+    validate do |value|
+      unless value.kind_of?(String)
+        fail("Invalid value '#{value}'. Should be a string")
+      end
+    end
+  end
+
+  # Name:         Ensure
+  # Type:         string
+  # IsMandatory:  False
+  # Values:       ["Present", "Absent"]
+  newparam(:dsc_ensure) do
+    def mof_type; 'string' end
+    def mof_is_embedded?; false end
+    desc "Ensure - Valid values are Present, Absent."
+    validate do |value|
+      resource[:ensure] = value.downcase
+      unless value.kind_of?(String)
+        fail("Invalid value '#{value}'. Should be a string")
+      end
+      unless ['Present', 'present', 'Absent', 'absent'].include?(value)
+        fail("Invalid value '#{value}'. Valid values are Present, Absent")
+      end
+    end
+  end
+
+  # Name:         Members
+  # Type:         string[]
+  # IsMandatory:  False
+  # Values:       None
+  newparam(:dsc_members, :array_matching => :all) do
+    def mof_type; 'string[]' end
+    def mof_is_embedded?; false end
+    desc "Members"
+    validate do |value|
+      unless value.kind_of?(Array) || value.kind_of?(String)
+        fail("Invalid value '#{value}'. Should be a string or an array of strings")
+      end
+    end
+    munge do |value|
+      Array(value)
+    end
+  end
+
+  # Name:         Create
+  # Type:         boolean
+  # IsMandatory:  False
+  # Values:       None
+  newparam(:dsc_create) do
+    def mof_type; 'boolean' end
+    def mof_is_embedded?; false end
+    desc "Create"
+    validate do |value|
+    end
+    newvalues(true, false)
+    munge do |value|
+      PuppetX::Dsc::TypeHelpers.munge_boolean(value.to_s)
+    end
+  end
+
+  # Name:         Purge
+  # Type:         boolean
+  # IsMandatory:  False
+  # Values:       None
+  newparam(:dsc_purge) do
+    def mof_type; 'boolean' end
+    def mof_is_embedded?; false end
+    desc "Purge"
+    validate do |value|
+    end
+    newvalues(true, false)
+    munge do |value|
+      PuppetX::Dsc::TypeHelpers.munge_boolean(value.to_s)
+    end
+  end
+
+  # Name:         DesiredState
+  # Type:         boolean
+  # IsMandatory:  False
+  # Values:       None
+  newparam(:dsc_desiredstate) do
+    def mof_type; 'boolean' end
+    def mof_is_embedded?; false end
+    desc "DesiredState"
+    validate do |value|
+    end
+    newvalues(true, false)
+    munge do |value|
+      PuppetX::Dsc::TypeHelpers.munge_boolean(value.to_s)
+    end
+  end
+
+
+  def builddepends
+    pending_relations = super()
+    PuppetX::Dsc::TypeHelpers.ensure_reboot_relationship(self, pending_relations)
+  end
+end
+
+Puppet::Type.type(:dsc_cmisc_localgroup2).provide :powershell, :parent => Puppet::Type.type(:base_dsc).provider(:powershell) do
+  confine :true => (Gem::Version.new(Facter.value(:powershell_version)) >= Gem::Version.new('5.0.10240.16384'))
+  defaultfor :operatingsystem => :windows
+
+  mk_resource_methods
+end
---
+++ lib/puppet_x/dsc_resources/cMisc/DSCResources/cMisc_LocalGroup2/cMisc_LocalGroup2.psm1
@@ -0,0 +1,122 @@
+﻿Function Get-TargetResource {
+
+    Param(
+        
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Group,
+
+        [Parameter(Mandatory=$True)]
+        [ValidateSet("Present", "Absent")]
+        [string]$Ensure,
+
+        [Parameter(Mandatory=$False)]
+        [string[]]$Members,
+
+        [Parameter(Mandatory=$False)]
+        [bool]$Create = $False,
+
+        [Parameter(Mandatory=$False)]
+        [bool]$Purge = $False,
+
+        [Parameter(Mandatory=$False)]
+        [bool]$DesiredState
+
+    )
+
+    $Members = $Members | % { If ($PSItem -match "\\") { $Split = $PSItem -split '\\' ; "WinNT://$($Split[0])/$($Split[1])" } Else { "WinNT://$env:COMPUTERNAME/$PSItem" } }
+    $ADSI = [ADSI]("WinNT://$env:COMPUTERNAME")    
+    $ADSIGroup = Try { $ADSI.Children.Find($Group,'group') ; $Exists = $True } Catch { $Exists = $False }
+    $CurrentMembers = If ($Exists) { $ADSIGroup.PSBase.Invoke('members') | % { $PSItem.GetType().InvokeMember("AdsPath","GetProperty",$Null,$PSItem,$Null) } } Else { $Null }
+    If ($Exists -and $Group -eq 'Administrators') { $CurrentMembers = $CurrentMembers | ? { -not $PSItem.EndsWith('/Administrator')} }
+    If ($CurrentMembers -ne $Null) { $Comparison = @(Compare-Object -ReferenceObject $Members -DifferenceObject $CurrentMembers) }
+    Else { $Comparison = "NoMembers" }
+    Return @{
+        RequiredMembers = $Members
+        CurrentMembers = $CurrentMembers
+        Group = $ADSIGroup
+        Exists = $Exists
+        Comparison = $Comparison
+    }
+}
+
+Function Set-TargetResource {
+    
+    Param(
+        
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Group,
+
+        [Parameter(Mandatory=$True)]
+        [ValidateSet("Present", "Absent")]
+        [string]$Ensure,
+
+        [Parameter(Mandatory=$False)]
+        [string[]]$Members,
+
+        [Parameter(Mandatory=$False)]
+        [bool]$Create = $False,
+
+        [Parameter(Mandatory=$False)]
+        [bool]$Purge = $False,
+
+        [Parameter(Mandatory=$False)]
+        [bool]$DesiredState
+
+    )
+
+    $CurrentState = Get-TargetResource @PSBoundParameters
+
+    If     ($Create -and -not $CurrentState.Exists) { ([ADSI]"WinNT://$env:COMPUTERNAME").Create("Group","Test").SetInfo() ; $CurrentState.Group = [ADSI]("WinNT://$env:COMPUTERNAME").Children.Find($Group,'group') }
+    If     ($CurrentState.Comparison -eq "NoMembers" -and $Ensure -eq "Present") { $CurrentState.RequiredMembers | % { $CurrentState.Group.Add($PSItem) } }
+    Elseif ($Ensure -eq "Present") {
+        $CurrentState.Comparison | Where SideIndicator -eq "<=" | Select -ExpandProperty InputObject | % { $CurrentState.Group.Add($PSItem) }
+        If ($Purge) { $CurrentState.Comparison | Where SideIndicator -eq "=>" | Select -ExpandProperty InputObject | % { $CurrentState.Group.Remove($PSItem) } }
+    }
+    Else { $CurrentState.CurrentMembers | % { If ($PSItem -in @($CurrentState.RequiredMembers)) { $CurrentState.Group.Remove($PSItem) } } }
+
+}
+
+Function Test-TargetResource {
+    
+    Param(
+        
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Group,
+
+        [Parameter(Mandatory=$True)]
+        [ValidateSet("Present", "Absent")]
+        [string]$Ensure,
+
+        [Parameter(Mandatory=$False)]
+        [string[]]$Members,
+
+        [Parameter(Mandatory=$False)]
+        [bool]$Create = $False,
+
+        [Parameter(Mandatory=$False)]
+        [bool]$Purge = $False,
+
+        [Parameter(Mandatory=$False)]
+        [bool]$DesiredState
+
+    )    
+
+    Write-Verbose "Nieuw2"
+    $CurrentState = Get-TargetResource @PSBoundParameters
+    
+    If     ($Create -and -not $CurrentState.Exists) { Return $False }
+    Elseif ($CurrentState.Comparison -eq "NoMembers" -and $Ensure -eq "Present") { Return $False }
+    Elseif ($CurrentState.Comparison -eq "NoMembers" -and $Ensure -eq "Absent") { Return $True }
+    Elseif ($Ensure -eq "Present") {
+        If (@($CurrentState.Comparison | Where SideIndicator -eq "<=").Count -gt 0) { Return $False }
+        Elseif (@($CurrentState.Comparison | Where SideIndicator -eq "=>").Count -gt 0 -and $Purge) { Return $False }
+        Else { Return $True }
+    }
+    Else {
+        If (@($CurrentState.Comparison | Where SideIndicator -eq "<=").Count -lt $CurrentState.RequiredMembers.Count) { Return $False }
+        Else { Return $True }
+    }
+}
